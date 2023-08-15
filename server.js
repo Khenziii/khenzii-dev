2b60c2290365688ccee4356c1c161cc761b1549a
Khenzii <khenzii.was.taken@gmail.com>
2023-08-15 16:01:07 +0200

    fixed the robots.txt

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use('/icons', express.static(path.join(__dirname, 'icons')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/videos', express.static(path.join(__dirname, 'videos')));
-app.use('/robots.txt', express.static(path.join(__filename, 'robots.txt')));
+app.use('/robots.txt', express.static(path.join(__dirname, 'robots.txt')));
+
 
 // '/' route
 app.get('/', (req, res) => {
