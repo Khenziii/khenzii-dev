120914bcdc01867989d9299b77b54e339d95f128
Khenzii <khenzii.was.taken@gmail.com>
2023-08-19 18:36:18 +0200

    made the logs even better

--- server.js
+++ server.js
@@ -37,59 +37,70 @@ app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/videos', express.static(path.join(__dirname, 'videos')));
 app.use('/robots.txt', express.static(path.join(__dirname, 'robots.txt')));
 
+app.use(function (req, res, next) {
+    var ClientIP = req.headers["Client-IP"];
+    if (ClientIP) {
+        req.ip = ClientIP;
+    } else {
+        req.ip = "<empty_string>"
+    }
+
+    next();
+});
+
 
 // '/' route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'index.html'));
-    consoleInfo("some user requested the '/' route")
+    consoleInfo(`${req.ip} requested the '/' route`)
 });
 
 // '/temp' route
 app.get('/temp', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'temp_mobile_index.html'));
-    consoleInfo("some user requested the '/temp' route")
+    consoleInfo(`${req.ip} requested the '/temp' route`)
 });
 
 // '/mobileinfo' route
 app.get('/mobileinfo', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'errors', 'page_not_mobile.html'));
-    consoleInfo("some user requested the '/mobileinfo' route")
+    consoleInfo(`${req.ip} requested the '/mobileinfo' route`)
 });
 
 // '/blog' route
 app.get('/blog', (req, res) => {
     // res.sendFile(path.join(__dirname, 'html', 'pages', 'blog', 'blog.html'));
     // uncomment the above line later, right now the /blog page is
-    // still being build so the route shows the "this page is still
-    // being build" html
+    // still being build so the route shows the `this page is still
+    // being build` html
     res.sendFile(path.join(__dirname, 'html', 'errors', 'page_being_build.html'));
-    consoleInfo("some user requested the '/blog' route")
+    consoleInfo(`${req.ip} requested the '/blog' route`)
 });
 
 // '/projects' route
 app.get('/projects', (req, res) => {
     // res.sendFile(path.join(__dirname, 'html', 'pages', 'projects', 'projects.html'));
     // uncomment the above line later, right now the /projects page 
-    // is still being build so the route shows the "this page is
-    // still being build" html
+    // is still being build so the route shows the `this page is
+    // still being build` html
     res.sendFile(path.join(__dirname, 'html', 'errors', 'page_being_build.html'));
-    consoleInfo("some user requested the '/projects' route")
+    consoleInfo(`${req.ip} requested the '/projects' route`)
 });
 
 // '/freebobux' route
 app.get('/freebobux', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'pages', 'freebobux', 'freebobux.html'));
-    consoleInfo("some user requested the '/freebobux' route :D")
+    consoleInfo(`${req.ip} requested the '/freebobux' route :D`)
 });
 
 // '/snake' route
 app.get('/snake', (req, res) => {
     // res.sendFile(path.join(__dirname, 'html', 'pages', 'snake', 'snake.html'));
     // uncomment the above line later, right now the /snake page
-    // is still being build so the route shows the "this page is
-    // still being build" html
+    // is still being build so the route shows the `this page is
+    // still being build` html
     res.sendFile(path.join(__dirname, 'html', 'errors', 'page_being_build.html'));
-    consoleInfo("some user requested the '/snake' route")
+    consoleInfo(`${req.ip} requested the '/snake' route`)
 });
 
 // '/page_being_build' route
@@ -102,7 +113,7 @@ app.get('/page_being_build', (req, res) => {
     // they are being worked on because it gives the user more info
     // redirecting can be confusing)
     res.sendFile(path.join(__dirname, 'html', 'errors', 'page_being_build.html'));
-    consoleInfo("some user requested the '/page_being_build' route")
+    consoleInfo(`${req.ip} requested the '/page_being_build' route`)
 });
 
 // Place routes above this comment!!!
@@ -110,13 +121,13 @@ app.get('/page_being_build', (req, res) => {
 // 404 error
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'html', 'errors', 'error_404.html'));
-    consoleInfo("some user got the 404 error")
+    consoleInfo(`${req.ip} got the 404 error`)
 });
 
 // Default error handler
 app.use((err, req, res, next) => {
     res.status(500).send('Bruh, something went wrong :P Check console for more Details. Sorry.');
-    consoleInfo(`some internal server error ocurred :/. Here is the error: ${err.stack}`)
+    consoleInfo(`ClientIP: ${req.ip}. Some internal server error ocurred :/. Here is the error: ${err.stack}`)
 });
 
 // Place errors above this comment!
