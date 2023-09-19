d1e3ef6425239648234f7a71d48a4ffd52039847
Khenzii <khenzii.was.taken@gmail.com>
2023-09-19 15:00:19 +0200

    added appropiate transitions to the top buttons, ima clean the workplace now

---
+++ js/pages/projects/swup_import_temp.js
@@ -0,0 +1,8 @@
+// delete this file later
+console.log("hello world!")
+import Swup from 'https://unpkg.com/swup@4.0.0-rc.21?module';
+
+const swup = new Swup({
+  containers: ["#swup"],
+  plugins: []
+});
