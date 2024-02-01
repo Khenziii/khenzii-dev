1e6b20f159191384ce40533bc09a14c34da0ffda
Khenziii <khenzii.was.taken@gmail.com>
2024-02-01 12:40:08 +0100

    rm -rf khenzii-dev
    sometimes, you gotta make a step back in order to continue moving
    forward :)

--- js/utils/buttons.js
+++
@@ -1,7 +0,0 @@
-function redirectTo(url, event) {
-    if (event.ctrlKey || event.metaKey) {
-        window.open(url, '_blank'); // Open in a new tab or window
-    } else {
-        window.location.href = url; // Open in the same tab
-    }
-}
--- js/utils/mobileinfo.js
+++
@@ -1,7 +0,0 @@
-function showPage(url) {
-    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-    if (screenWidth <= 800) {
-        window.location.href = url;
-    }
-}
