7fb676bf785b2c77a73ae445a904b97bdcc6d801
Khenziii <khenzii.was.taken@gmail.com>
2024-02-06 18:02:11 +0100

    Revert "rename .js files to .ts"
    This reverts commit 7d9a68a5370883554d2b527d6f43021948b77d98.
    
    Guess what, xD. There was some deep reason behind those .js files.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-import { NextConfig } from "next";
-
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("@khenzii-dev/env");
+await import("./src/env.js");
 
-const config: NextConfig = {};
+/** @type {import("next").NextConfig} */
+const config = {};
 
 export default config;
