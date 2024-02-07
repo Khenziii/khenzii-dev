90a26f9a063e0978d57424c1e38d6b43d7ce4424
Khenziii <khenzii.was.taken@gmail.com>
2024-02-07 15:43:45 +0100

    replace old paragraphs

--- src/ui/atoms/paragraph/index.tsx
+++ src/ui/atoms/paragraph/index.tsx
@@ -3,10 +3,11 @@ import style from "./index.module.scss";
 
 export type ParagraphProps = {
     children?: ReactNode;
+    fontSize?: number;
 };
 
-export const Paragraph: FC<ParagraphProps> = ({ children,  }) => (
-    <p className={style.paragraph}>
+export const Paragraph: FC<ParagraphProps> = ({ children, fontSize = 1 }) => (
+    <p className={style.paragraph} style={{ fontSize: `${fontSize}rem` }}>
         {children}
     </p>
 );
--- src/ui/ogranisms/footer/index.tsx
+++ src/ui/ogranisms/footer/index.tsx
@@ -1,18 +1,17 @@
 import { type FC } from "react";
 import style from "./index.module.scss";
-import clsx from "clsx";
-import { Flex } from "@khenzii-dev/ui/atoms";
+import { Flex, Paragraph } from "@khenzii-dev/ui/atoms";
 
 export type FooterProps = {};
 
 export const Footer: FC<FooterProps> = ({}) => (
     <Flex direction={"column"} gap={10} align={"center"}>
-        <p>
+        <Paragraph fontSize={1.25}>
             Made with 🤍 by Khenzii
-        </p>
+        </Paragraph>
 
-        <p>
+        <Paragraph>
             All of the code is available in <a href={"https://github.com/Khenziii/khenzii-dev"}>website's github repo</a>. Licensed under <a href={"https://github.com/Khenziii/khenzii-dev/blob/master/LICENSE"}>CC-BY-SA-4.0</a>.
-        </p>
+        </Paragraph>
     </Flex>
 );
