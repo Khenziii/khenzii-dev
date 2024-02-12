ef25e6f25803834afb1c4ee603a7d91c68dc8b25
Khenziii <khenzii.was.taken@gmail.com>
2024-02-12 19:09:57 +0100

    hotfix(ui): replace useRouter with usePathname
    I forgor that I'm using the new app directory xD

--- src/ui/organisms/nav/nav_desktop/index.tsx
+++ src/ui/organisms/nav/nav_desktop/index.tsx
@@ -3,19 +3,19 @@ import { type route } from "..";
 import style from "./index.module.scss";
 import { NavDesktopItem } from "./nav_desktop_item";
 import { Flex } from "@khenzii-dev/ui/atoms";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 export type NavDesktopProps = {
     routes: route[];
 };
 
 export const NavDesktop: FC<NavDesktopProps> = ({ routes }) => {
-    const router = useRouter();
+    const pathname = usePathname();
 
     return (
         <Flex align={"center"} justify={"space-around"} className={style.container}>
             {routes.map((r, index) => (
-                <NavDesktopItem route={r} key={`nav-desktop-item-${index}`} />
+                <NavDesktopItem route={r} key={`nav-desktop-item-${index}`} active={pathname === r.path} />
             ))}
         </Flex>
     );
--- src/ui/organisms/nav/nav_mobile/index.tsx
+++ src/ui/organisms/nav/nav_mobile/index.tsx
@@ -1,19 +1,19 @@
 import { type FC } from "react";
 import { type route } from "..";
 import { NavMobileItem } from "./nav_mobile_item";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 export type NavMobileProps = {
     routes: route[];
 };
 
 export const NavMobile: FC<NavMobileProps> = ({ routes }) => {
-    const router = useRouter();
+    const pathname = usePathname();
 
     return (
         <>
             {routes.map((r, index) => (
-                <NavMobileItem route={r} key={`nav-mobile-item-${index}`} active={router.pathname === r.path} />
+                <NavMobileItem route={r} key={`nav-mobile-item-${index}`} active={pathname === r.path} />
             ))}
         </>
     );
