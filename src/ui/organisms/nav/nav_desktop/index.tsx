a06f18f4b97fde62199de5a1cae599d189e9fe91
Khenziii <khenzii.was.taken@gmail.com>
2024-02-12 19:04:06 +0100

    feat(ui): pass needed active prop

--- src/ui/organisms/nav/nav_desktop/index.tsx
+++ src/ui/organisms/nav/nav_desktop/index.tsx
@@ -3,15 +3,20 @@ import { type route } from "..";
 import style from "./index.module.scss";
 import { NavDesktopItem } from "./nav_desktop_item";
 import { Flex } from "@khenzii-dev/ui/atoms";
+import { useRouter } from "next/router";
 
 export type NavDesktopProps = {
     routes: route[];
 };
 
-export const NavDesktop: FC<NavDesktopProps> = ({ routes }) => (
-    <Flex align={"center"} justify={"space-around"} className={style.container}>
-        {routes.map((r, index) => (
-            <NavDesktopItem route={r} key={`nav-desktop-item-${index}`} />
-        ))}
-    </Flex>
-);
+export const NavDesktop: FC<NavDesktopProps> = ({ routes }) => {
+    const router = useRouter();
+
+    return (
+        <Flex align={"center"} justify={"space-around"} className={style.container}>
+            {routes.map((r, index) => (
+                <NavDesktopItem route={r} key={`nav-desktop-item-${index}`} />
+            ))}
+        </Flex>
+    );
+};
--- src/ui/organisms/nav/nav_mobile/index.tsx
+++ src/ui/organisms/nav/nav_mobile/index.tsx
@@ -1,15 +1,20 @@
 import { type FC } from "react";
 import { type route } from "..";
 import { NavMobileItem } from "./nav_mobile_item";
+import { useRouter } from "next/router";
 
 export type NavMobileProps = {
     routes: route[];
 };
 
-export const NavMobile: FC<NavMobileProps> = ({ routes }) => (
-    <>
-        {routes.map((r, index) => (
-            <NavMobileItem route={r} key={`nav-mobile-item-${index}`} />
-        ))}
-    </>
-);
+export const NavMobile: FC<NavMobileProps> = ({ routes }) => {
+    const router = useRouter();
+
+    return (
+        <>
+            {routes.map((r, index) => (
+                <NavMobileItem route={r} key={`nav-mobile-item-${index}`} active={router.pathname === r.path} />
+            ))}
+        </>
+    );
+};
