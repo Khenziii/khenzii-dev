fcd2ab23781a6063f0dea76c1364260c56972bfa
Khenzii <126098761+Khenziii@users.noreply.github.com>
2024-02-16 17:12:39 +0100

    feat(ui): mobile nav (#36)

--- src/ui/organisms/nav/nav_mobile/index.tsx
+++ src/ui/organisms/nav/nav_mobile/index.tsx
@@ -1,5 +1,6 @@
 import { type FC } from "react";
 import { type route } from "..";
+import style from "./index.module.scss";
 import { NavMobileItem } from "./nav_mobile_item";
 import { usePathname } from "next/navigation";
 
@@ -11,10 +12,10 @@ export const NavMobile: FC<NavMobileProps> = ({ routes }) => {
     const pathname = usePathname();
 
     return (
-        <>
+        <div className={style.container}>
             {routes.map((r, index) => (
                 <NavMobileItem route={r} key={`nav-mobile-item-${index}`} active={pathname === r.path} />
             ))}
-        </>
+        </div>
     );
 };
