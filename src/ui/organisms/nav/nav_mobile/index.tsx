307794048fe463bd62e0ac21acf5dfdafc6ddaea
Khenziii <khenzii.was.taken@gmail.com>
2024-02-16 16:28:06 +0100

    feat(ui): remove useless flex component

--- src/ui/organisms/nav/nav_mobile/index.tsx
+++ src/ui/organisms/nav/nav_mobile/index.tsx
@@ -2,7 +2,6 @@ import { type FC } from "react";
 import { type route } from "..";
 import style from "./index.module.scss";
 import { NavMobileItem } from "./nav_mobile_item";
-import { Flex } from "@khenzii-dev/ui/atoms";
 import { usePathname } from "next/navigation";
 
 export type NavMobileProps = {
@@ -13,10 +12,10 @@ export const NavMobile: FC<NavMobileProps> = ({ routes }) => {
     const pathname = usePathname();
 
     return (
-        <Flex align={"center"} justify={"space-around"} className={style.container}>
+        <div className={style.container}>
             {routes.map((r, index) => (
                 <NavMobileItem route={r} key={`nav-mobile-item-${index}`} active={pathname === r.path} />
             ))}
-        </Flex>
+        </div>
     );
 };
