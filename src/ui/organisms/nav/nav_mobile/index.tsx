0b0b6cfa8fec0bf4ebe9e34fa54dc60f2efcc783
Khenziii <khenzii.was.taken@gmail.com>
2024-02-16 16:26:12 +0100

    feat(ui): change mobile nav's styles

--- src/ui/organisms/nav/nav_mobile/index.tsx
+++ src/ui/organisms/nav/nav_mobile/index.tsx
@@ -1,6 +1,8 @@
 import { type FC } from "react";
 import { type route } from "..";
+import style from "./index.module.scss";
 import { NavMobileItem } from "./nav_mobile_item";
+import { Flex } from "@khenzii-dev/ui/atoms";
 import { usePathname } from "next/navigation";
 
 export type NavMobileProps = {
@@ -11,10 +13,10 @@ export const NavMobile: FC<NavMobileProps> = ({ routes }) => {
     const pathname = usePathname();
 
     return (
-        <>
+        <Flex align={"center"} justify={"space-around"} className={style.container}>
             {routes.map((r, index) => (
                 <NavMobileItem route={r} key={`nav-mobile-item-${index}`} active={pathname === r.path} />
             ))}
-        </>
+        </Flex>
     );
 };
