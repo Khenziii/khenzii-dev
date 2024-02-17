13eb9355f08fbf882c1d30cee15520b935e27152
Khenziii <khenzii.was.taken@gmail.com>
2024-02-17 14:11:08 +0100

    feat(website): fill out the socials array

--- src/ui/organisms/nav/index.tsx
+++ src/ui/organisms/nav/index.tsx
@@ -18,8 +18,28 @@ export type route = {
 
 export const socials: social[] = [
     {
+        iconName: "Steam",
+        url: "https://steamcommunity.com/id/khenzii/",
+    },
+    {
+        iconName: "Discord",
+        url: "https://discord.com/invite/5JcGp5gRkn",
+    },
+    {
         iconName: "Github",
         url: "https://github.com/Khenziii",
+    },
+    {
+        iconName: "Twitter",
+        url: "https://twitter.com/kheenziii",
+    },
+    {
+        iconName: "Spotify",
+        url: "https://open.spotify.com/user/31yudiyuep2e25bhirxlhavfang4",
+    },
+    {
+        iconName: "Youtube",
+        url: "https://www.youtube.com/@kheenzii",
     },
 ]
 
