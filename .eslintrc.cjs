f4e9f8daaacb2d5345bb9e2d0118f4419be0317c
Khenziii <khenzii.was.taken@gmail.com>
2024-02-25 14:37:54 +0100

    feat(ui): run `yarn dlx storybook@latest init --builder webpack5`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:storybook/recommended"
   ],
   rules: {
     "@typescript-eslint/array-type": "off",
