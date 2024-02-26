83fff78e5aaad5e0ae623a2a089eab1ef9e2378a
Khenziii <khenzii.was.taken@gmail.com>
2024-02-26 17:38:24 +0100

    feat(ui): add Logo's story

---
+++ src/ui/atoms/logo/index.stories.tsx
@@ -0,0 +1,15 @@
+import { Logo, type LogoProps } from "@khenzii-dev/ui/atoms";
+import { type StoriesType } from "@khenzii-dev/ui/types/stories-type";
+
+export default {
+    component: Logo,
+    title: 'Logo',
+    tags: ['autodocs'],
+};
+
+export const Default: StoriesType<LogoProps> = {
+    args: {
+        size: 100,
+        animate: true,
+    }
+};
