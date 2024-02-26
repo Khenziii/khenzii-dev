72d211df0d2d0fb12901241c00bd61ed4db964cc
Khenziii <khenzii.was.taken@gmail.com>
2024-02-26 18:05:38 +0100

    feat(ui): add NavDesktop's story

---
+++ src/ui/organisms/nav/nav_desktop/index.stories.tsx
@@ -0,0 +1,33 @@
+import { NavDesktop, type NavDesktopProps } from ".";
+import { type StoriesType } from "@khenzii-dev/ui/types/stories-type";
+
+export default {
+    component: NavDesktop,
+    title: 'NavDesktop',
+    tags: ['autodocs'],
+};
+
+export const Default: StoriesType<NavDesktopProps> = {
+    args: {
+        routes: [
+            {
+                name: "route",
+                path: "#",
+            },
+            {
+                name: "thing",
+                path: "#",
+            },
+        ],
+        socials: [
+            {
+                url: "#",
+                iconName: "Github",
+            },
+            {
+                url: "#",
+                iconName: "Facebook",
+            },
+        ],
+    },
+};
