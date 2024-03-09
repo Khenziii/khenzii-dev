ece9acef4572841f6ff6f9a9fc9f83f495a48e7c
Khenziii <khenzii.was.taken@gmail.com>
2024-03-09 23:07:12 +0100

    feat: add comma-dangle rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,5 +47,6 @@ module.exports = {
     "@stylistic/semi": ["error", "always"],
     "@stylistic/function-paren-newline": ["error", { "minItems": 4 }],
     "@stylistic/function-call-argument-newline": ["error", "consistent"],
+    "@stylistic/comma-dangle": ["error", "always-multiline"],
   },
 };
