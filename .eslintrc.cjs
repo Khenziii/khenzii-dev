504bfe4f141c88715693b5a5d26ab83c85965e3c
Khenziii <khenzii.was.taken@gmail.com>
2024-03-09 22:53:22 +0100

    feat: add function-paren-newline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -45,5 +45,6 @@ module.exports = {
     }],
     "@stylistic/object-curly-spacing": ["error", "always"],
     "@stylistic/semi": ["error", "always"],
+    "@stylistic/function-paren-newline": ["error", { "minItems": 4 }],
   },
 };
