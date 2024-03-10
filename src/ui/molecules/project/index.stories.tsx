19fba7d5e1be0d9d2acf0021da4e7504863d0436
Khenziii <khenzii.was.taken@gmail.com>
2024-03-10 12:25:44 +0100

    feat: create projects component

--- src/ui/molecules/project/index.stories.tsx
+++ src/ui/molecules/project/index.stories.tsx
@@ -1,1 +1,22 @@
+import { Project, type ProjectProps } from "@khenzii-dev/ui/molecules";
+import { type StoriesType } from "@khenzii-dev/ui/types/stories-type";
+import { Icon } from "@khenzii-dev/ui/atoms";
 
+export default {
+    component: Project,
+    title: 'Project',
+    tags: ['autodocs'],
+};
+
+export const Default: StoriesType<ProjectProps> = {
+    args: {
+        name: "khenzii.dev",
+        description: "The very project that you're working on right now!",
+        backgroundColor: "#000000",
+        githubRepoUrl: "https://github.com/Khenziii/khenzii-dev",
+        websiteUrl: "https://khenzii.dev/",
+        startedWorking: new Date(new Date().getTime() - 1000 * 60 * 60),
+        finishedWorking: new Date(),
+        topLeftComponent: <Icon iconName={"globe2"} size={2} />,
+    },
+};
--- src/ui/molecules/project/index.tsx
+++ src/ui/molecules/project/index.tsx
@@ -1,1 +1,36 @@
+import { type FC, type ReactNode } from "react";
+import { Flex } from "@khenzii-dev/ui/atoms";
+import { Paragraph } from "@khenzii-dev/ui/atoms";
+import style from "./index.module.scss";
+import clsx from "clsx";
 
+export type ProjectProps = {
+    name: string;
+    description: string;
+    backgroundColor: string;
+    topLeftComponent: ReactNode;
+    startedWorking: Date;
+    finishedWorking?: Date;
+    githubRepoUrl?: string;
+    websiteUrl?: string;
+};
+
+export const Project: FC<ProjectProps> = ({ name, description, backgroundColor, topLeftComponent, startedWorking, finishedWorking, githubRepoUrl, websiteUrl }) => {
+
+
+    return (
+        <Flex direction={"column"} justify={"center"} fullWidth>
+            <Flex direction={"row"} align={"center"} fullWidth className={style.title}>
+                {topLeftComponent}
+
+                <Paragraph fontSize={2}>
+                    {name}
+                </Paragraph>
+            </Flex>
+
+            <Paragraph>
+                {description}
+            </Paragraph>
+        </Flex>
+    );
+};
