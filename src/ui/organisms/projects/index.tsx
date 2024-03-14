c2ff71e40ff8ee1221aa8b96646fd075698dade7
Khenziii <khenzii.was.taken@gmail.com>
2024-03-14 18:09:43 +0100

    feat(ui): move gradient to a underline

--- src/ui/organisms/projects/index.tsx
+++ src/ui/organisms/projects/index.tsx
@@ -7,7 +7,6 @@ export type project = {
     name: string;
     description: ReactNode;
     backgroundGradient: string;
-    secondaryColor: string;
     topLeftComponent: ReactNode;
     startedWorking: Date;
     finishedWorking?: Date;
@@ -22,30 +21,27 @@ export const projects: project[] = [
             ... This should be filled out later..
         </Paragraph>,
         backgroundGradient: "linear-gradient(270deg, #FFFFFF, #20201f)",
-        secondaryColor: "#20201f",
         githubRepoUrl: "https://github.com/Khenziii/khenzii-dev",
         websiteUrl: "https://khenzii.dev/",
         startedWorking: new Date(new Date().getTime() - 1000 * 60 * 60),
         topLeftComponent: <Icon iconName={"globe2"} size={2} />,
     },
-    {
-        name: "khenz-tiktokbot",
-        description: <Paragraph>
-            ... This should be filled out later..
-        </Paragraph>,
-        backgroundGradient: "linear-gradient(270deg, #FF0050, #00F2EA)",
-        secondaryColor: "#000000",
-        githubRepoUrl: "https://github.com/Khenziii/khenz-tiktokbot",
-        startedWorking: new Date(new Date().getTime() - 1000 * 60 * 60),
-        topLeftComponent: <Icon iconName={"robot"} size={2} />,
-    },
+    // {
+    //     name: "khenz-tiktokbot",
+    //     description: <Paragraph>
+    //         ... This should be filled out later..
+    //     </Paragraph>,
+    //     backgroundGradient: "linear-gradient(270deg, #FF0050, #00F2EA)",
+    //     githubRepoUrl: "https://github.com/Khenziii/khenz-tiktokbot",
+    //     startedWorking: new Date(new Date().getTime() - 1000 * 60 * 60),
+    //     topLeftComponent: <Icon iconName={"robot"} size={2} />,
+    // },
     {
         name: "lol-cup",
         description: <Paragraph>
             ... This should be filled out later..
         </Paragraph>,
         backgroundGradient: "linear-gradient(270deg, #1AFFAF, #111112)",
-        secondaryColor: "#000000",
         websiteUrl: "https://lolcup.zsl.gda.pl/",
         startedWorking: new Date(new Date().getTime() - 1000 * 60 * 60),
         topLeftComponent: <Image
--- src/ui/organisms/projects/project/index.tsx
+++ src/ui/organisms/projects/project/index.tsx
@@ -6,61 +6,62 @@ import { Expandable, GitHubRepoCard } from "@khenzii-dev/ui/molecules";
 import style from "./index.module.scss";
 import { type project } from "..";
 
-export const Project: FC<project> = ({ name, description, backgroundGradient, secondaryColor, topLeftComponent, startedWorking, finishedWorking, githubRepoUrl, websiteUrl }) => {
+export const Project: FC<project> = ({ name, description, backgroundGradient, topLeftComponent, startedWorking, finishedWorking, githubRepoUrl, websiteUrl }) => {
     const [isExpanded, setIsExpanded] = useState(false);
 
     return (
         <Flex direction={"column"} justify={"center"} gap={0} className={style.container}>
-            <Flex 
-                direction={"row"} 
-                align={"center"} 
-                fullWidth 
-                className={style.title}
-                styles={{ backgroundImage: backgroundGradient }}
-                gap={10}
-            >
-                <Flex direction={"row"} justify={"flex-start"} align={"center"}>
-                    {topLeftComponent}
-
-                    <Paragraph fontSize={2} styles={{ whiteSpace: "nowrap" }}>
-                        {name}
-                    </Paragraph>
-                </Flex>
-
+            <Flex direction={"column"} justify={"center"} gap={0} fullWidth className={style.title}>
                 <Flex
                     direction={"row"}
-                    justify={"flex-end"}
                     align={"center"}
-                    gap={20}
                     fullWidth
+                    gap={10}
                 >
-                    {websiteUrl && (
-                        <Anchor href={websiteUrl} prefetch={false} newTab className={style.darkenChildOnHover}>
-                            <Icon iconName={"link-45deg"} size={3} color={secondaryColor} />
-                        </Anchor>
-                    )}
+                    <Flex direction={"row"} justify={"flex-start"} align={"center"}>
+                        {topLeftComponent}
 
-                    {githubRepoUrl && (
-                        <Anchor href={githubRepoUrl} prefetch={false} newTab className={style.darkenChildOnHover}>
-                            <Icon iconName={"github"} size={3} color={secondaryColor} />
-                        </Anchor>
-                    )}
+                        <Paragraph fontSize={2} styles={{ whiteSpace: "nowrap" }}>
+                            {name}
+                        </Paragraph>
+                    </Flex>
 
-                    <div
-                        className={style.darkenChildOnHover}
-                        onClick={() => setIsExpanded((e) => !e)}
+                    <Flex
+                        direction={"row"}
+                        justify={"flex-end"}
+                        align={"center"}
+                        gap={20}
+                        fullWidth
                     >
-                        <Icon
-                            iconName={
-                                isExpanded
-                                ? "arrow-up-short"
-                                : "arrow-down-short"
-                            }
-                            size={3}
-                            color={secondaryColor}
-                        />
-                    </div>
+                        {websiteUrl && (
+                            <Anchor href={websiteUrl} prefetch={false} newTab className={style.darkenChildOnHover}>
+                                <Icon iconName={"link-45deg"} size={3} />
+                            </Anchor>
+                        )}
+
+                        {githubRepoUrl && (
+                            <Anchor href={githubRepoUrl} prefetch={false} newTab className={style.darkenChildOnHover}>
+                                <Icon iconName={"github"} size={3} />
+                            </Anchor>
+                        )}
+
+                        <div
+                            className={style.darkenChildOnHover}
+                            onClick={() => setIsExpanded((e) => !e)}
+                        >
+                            <Icon
+                                iconName={
+                                    isExpanded
+                                        ? "arrow-up-short"
+                                        : "arrow-down-short"
+                                }
+                                size={3}
+                            />
+                        </div>
+                    </Flex>
                 </Flex>
+
+                <div className={style.underline} style={{ backgroundImage: backgroundGradient, height: "10px" }} />
             </Flex>
 
             <Expandable
