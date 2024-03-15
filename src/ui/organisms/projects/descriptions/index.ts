45331012b8c84199bc6c2d6abd8705cf74b79ce0
Khenziii <khenzii.was.taken@gmail.com>
2024-03-15 22:27:37 +0100

    feat: add khenz-tiktokbot project

--- src/ui/organisms/projects/descriptions/index.ts
+++ src/ui/organisms/projects/descriptions/index.ts
@@ -1,2 +1,3 @@
 export * from "./khenzii-dev";
 export * from "./lol-cup";
+export * from "./khenz-tiktokbot";
--- src/ui/organisms/projects/index.tsx
+++ src/ui/organisms/projects/index.tsx
@@ -3,6 +3,7 @@ import { Icon } from "@khenzii-dev/ui/atoms";
 import { Project } from "./project";
 import Image from "next/image";
 import {
+    KhenzTiktokbotDescription,
     KhenziiDevDescription,
     LolCupDescription,
 } from "@khenzii-dev/ui/organisms/projects/descriptions";
@@ -25,6 +26,17 @@ export type project = {
 
 export const projects: project[] = [
     {
+        name: "khenz-tiktokbot",
+        description: <KhenzTiktokbotDescription />,
+        backgroundGradient: "linear-gradient(270deg, #FF0050, #00F2EA, #000000)",
+        githubRepoUrl: "https://github.com/Khenziii/khenz-tiktokbot",
+        dates: [
+            [new Date(2023, 4), new Date(2023, 7)],
+        ],
+        topLeftComponent: <Icon iconName={"robot"} size={2} />,
+        role: projectRole.FOUNDER,
+    },
+    {
         name: "khenzii.dev",
         description: <KhenziiDevDescription />,
         backgroundGradient: "linear-gradient(270deg, #FFFFFF, #20201f)",
@@ -32,21 +44,11 @@ export const projects: project[] = [
         websiteUrl: "https://khenzii.dev/",
         dates: [
             [new Date(2023, 7), new Date(2023, 9)],
-            [new Date(2024, 2)],
+            [new Date(2024, 1)],
         ],
         topLeftComponent: <Icon iconName={"globe2"} size={2} />,
         role: projectRole.FOUNDER,
     },
-    // {
-    //     name: "khenz-tiktokbot",
-    //     description: <Paragraph>
-    //         ... This should be filled out later..
-    //     </Paragraph>,
-    //     backgroundGradient: "linear-gradient(270deg, #FF0050, #00F2EA)",
-    //     githubRepoUrl: "https://github.com/Khenziii/khenz-tiktokbot",
-    //     startedWorking: new Date(new Date().getTime() - 1000 * 60 * 60),
-    //     topLeftComponent: <Icon iconName={"robot"} size={2} />,
-    // },
     {
         name: "lol-cup",
         description: <LolCupDescription />,
