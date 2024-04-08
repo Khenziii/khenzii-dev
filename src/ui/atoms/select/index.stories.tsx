383f9a7f3444697d2bab05f00cfb72ee777bd8dc
Khenziii <khenzii.was.taken@gmail.com>
2024-04-08 21:38:28 +0200

    feat(ui): add Select component boilerplate

---
+++ src/ui/atoms/select/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Select, type SelectProps } from "@khenzii-dev/ui/atoms";
+import { type StoriesType } from "@khenzii-dev/ui/types/stories-type";
+
+export default {
+    component: Select,
+    title: 'Select',
+    tags: ['autodocs'],
+};
+
+export const Default: StoriesType<SelectProps> = {
+    args: {},
+};
---
+++ src/ui/atoms/select/index.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+import style from "./index.module.scss";
+import clsx from "clsx";
+
+export type SelectProps = {};
+
+export const Select: FC<SelectProps> = () => {
+    return (
+        <p>...</p>
+    );
+};
