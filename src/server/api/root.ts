08764919d9bd33861df7832091f3de5a66ed3d54
Khenziii <khenzii.was.taken@gmail.com>
2024-04-18 18:01:43 +0200

    feat(api): add api & auth boilerplate

---
+++ src/server/api/root.ts
@@ -0,0 +1,22 @@
+import { createCallerFactory, createTRPCRouter } from "@khenzii-dev/server/api/trpc";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+    // example: exampleRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+/**
+ * Create a server-side caller for the tRPC API.
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
+export const createCaller = createCallerFactory(appRouter);
