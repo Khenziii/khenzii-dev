5c2efd6d542def24b84244d96b08fe2508f456ed
Khenziii <khenzii.was.taken@gmail.com>
2024-04-18 21:00:21 +0200

    refactor(api): reindent api.ts with 4 spaces

--- src/server/api.ts
+++ src/server/api.ts
@@ -10,12 +10,12 @@ import { createTRPCContext } from "@khenzii-dev/server/api/trpc";
  * handling a tRPC call from a React Server Component.
  */
 const createContext = cache(() => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
+    const heads = new Headers(headers());
+    heads.set("x-trpc-source", "rsc");
 
-  return createTRPCContext({
-    headers: heads,
-  });
+    return createTRPCContext({
+        headers: heads,
+    });
 });
 
 export const api = createCaller(createContext);
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -89,7 +89,7 @@ export const publicProcedure = t.procedure;
 /**
  * Protected (authenticated) procedure
  *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
+ * If you want a query or mutation to ONLY be accessible to logged-in users, use this. It verifies
  * the session is valid and guarantees `ctx.session.user` is not null.
  *
  * @see https://trpc.io/docs/procedures
