984892716ce617a5b5647288f8ec953c04764f05
Khenziii <khenzii.was.taken@gmail.com>
2024-04-24 18:31:19 +0200

    feat(website): homepage: add `About Me` section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,43 @@ const Home = () => (
         <Flex direction={"column"} align={"center"} gap={0}>
             <Logo animate={true} size={300} />
 
-            <Paragraph fontSize={2}>
+            <Paragraph fontSize={2.25}>
                 Khenzii
             </Paragraph>
 
-            <Paragraph fontSize={1.5} className={style.title}>
+            <Paragraph fontSize={1.75} className={style.title}>
                 Hobbyist Programmer
             </Paragraph>
+        </Flex>
+
+        <Paragraph fontSize={1.5}>
+            {"Hello! ^^. I'm Khenzii, and I create stuff."}
+        </Paragraph>
+
+        <Flex direction={"column"} align={"flex-start"} gap={0}>
+            {/*TODO: implement a `Header` component*/}
+            <Paragraph fontSize={1.5}>
+                About Me
+            </Paragraph>
+
+            <ul className={style.aboutList}>
+                <li>
+                    <Paragraph fontSize={1.25}>Open-Source enjoyer.</Paragraph>
+                </li>
+                <li>
+                    {/*TODO: implement calculating current Europe/Warsaw time, and create a `code` component*/}
+                    <Paragraph fontSize={1.25}>
+                        {"Based in Poland, Gdańsk; where it's currently "}
+                        <code>18:13</code>
+                        {"."}
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph fontSize={1.25}>
+                        High School student.
+                    </Paragraph>
+                </li>
+            </ul>
         </Flex>
     </Flex>
 );
