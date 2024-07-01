66328eef29893176a476b9cd2dcffcea4413fc37
Khenziii <khenzii.was.taken@gmail.com>
2024-07-01 16:23:42 +0200

    feat: create shell.nix
    Prisma has some issues with running on NixOS out of the box.

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodejs_22
+    prisma-engines
+  ];
+
+  shellHook = ''
+    export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+    export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+    export PATH="$PWD/node_modules/.bin/:$PATH"
+  '';
+}
+
