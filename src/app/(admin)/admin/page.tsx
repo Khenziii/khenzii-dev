69bc0c70c1ec136a2d5ec06f5e1cd14143d557c6
Khenziii <khenzii.was.taken@gmail.com>
2024-08-24 18:29:40 +0200

    feat(website): add basic admin panel
    I really need a desinger.

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -3,20 +3,78 @@
 import {
     Paragraph,
     Button,
+    Icon,
+    Flex,
+    Header,
+    CodeBlock,
 } from "@khenzii-dev/ui/atoms";
-import { signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
+import style from "@khenzii-dev/styles/admin.module.scss";
 
 const Admin = () => {
+    const { data: session } = useSession();
+    if (!session) return;
+
     return (
-        <>
-            <Paragraph fontSize={1.5}>{"You're logged in!"}</Paragraph>
-            <Button 
-                onClick={() => signOut({ redirect: false })}
-                style={{ width: "fit-content" }}
+        <Flex
+            direction="column"
+            gap={20}
+            className={style.container}
+        >
+            <Flex
+                direction="column"
+                align="center"
             >
-                <Paragraph>Logout</Paragraph>
-            </Button>
-        </>
+                <Header>Welcome, {session.user.name}!</Header>
+
+                <Flex direction="column" align="flex-start">
+                    <Paragraph fontSize={1.75}>Session Details:</Paragraph>
+                    <Paragraph fontSize={1.5}>
+                        <CodeBlock>
+                            Username: {session.user.name} <br />
+                            Email: {session.user.email} <br />
+                        </CodeBlock>
+                    </Paragraph>
+                </Flex>
+
+                <Button
+                    onClick={() => signOut({ redirect: false })}
+                    className={style.logout_button}
+                >
+                    <Flex direction="row" align="center">
+                        <Paragraph fontSize={1.5}>Logout</Paragraph>
+                        <Icon iconName={"box-arrow-right"} size={1.5} />
+                    </Flex>
+                </Button>
+            </Flex>
+
+            <Flex
+                direction="column"
+                align="center"
+            >
+                <Header>Current Project</Header>
+
+                <Paragraph fontSize={1.75}>This section is still being built..</Paragraph>
+            </Flex>
+
+            <Flex
+                direction="column"
+                align="center"
+            >
+                <Header>Blog</Header>
+
+                <Paragraph fontSize={1.75}>This section is still being built..</Paragraph>
+            </Flex>
+
+            <Flex
+                direction="column"
+                align="center"
+            >
+                <Header>Event Log</Header>
+
+                <Paragraph fontSize={1.75}>This section is still being built..</Paragraph>
+            </Flex>
+        </Flex>
     );
 };
 
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -22,6 +22,7 @@ declare module "next-auth" {
     interface User {
         id: string;
         email: string;
+        name: string;
     }
 }
 
