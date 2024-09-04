430fa55f327a91f60d174db56f4dd4ce1c41f8e4
Khenziii <khenzii.was.taken@gmail.com>
2024-09-04 19:54:04 +0200

    feat: update `shell.nix`

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs_22
     prisma-engines
+    cypress
   ];
 
   shellHook = ''
@@ -12,6 +13,8 @@ pkgs.mkShell {
     export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
     export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
     export PATH="$PWD/node_modules/.bin/:$PATH"
+    export CYPRESS_INSTALL_BINARY="0"
+    export CYPRESS_RUN_BINARY="${pkgs.cypress}/bin/Cypress"
   '';
 }
 
