342a5d5e501234f84ced5408ed76885661b56b15
Khenziii <khenzii.was.taken@gmail.com>
2024-09-07 13:40:55 +0200

    fix(ci-cd): make workflow fail if cypress tests failed

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -9,7 +9,10 @@ DEV_SERVER_PID=$!
 # Run cypress' tests.
 wait-on http://localhost:3000 --interval 5000 
 cypress run 
+CYPRESS_EXIT_CODE=$?
 
 # Shutdown the app.
 docker compose down 
 kill $DEV_SERVER_PID
+
+exit $CYPRESS_EXIT_CODE
