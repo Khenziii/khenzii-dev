feabd90f7c595825e64ccdd4f498cba0201d51ab
Khenziii <khenzii.was.taken@gmail.com>
2024-09-12 20:25:48 +0200

    fix: correctly skip authentication in testing xD

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+NODE_ENV=test
+
 # Start the app.
 yarn install
-docker compose up --detach 
+docker compose up --detach
 yarn dev &
 DEV_SERVER_PID=$!
 
 # Run cypress' tests.
-wait-on http://localhost:3000 --interval 5000 
-cypress run 
+wait-on http://localhost:3000 --interval 5000
+cypress run
 CYPRESS_EXIT_CODE=$?
 
 # Shutdown the app.
-docker compose down 
+docker compose down
 kill $DEV_SERVER_PID
 
 exit $CYPRESS_EXIT_CODE
