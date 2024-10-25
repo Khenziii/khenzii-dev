468f35c89015585211bcd3ac53a1c215c8be1d94
Khenziii <khenzii.was.taken@gmail.com>
2024-10-25 19:21:32 +0200

    refactor(ui): `fontsize` --> `fontSize`

--- src/ui/atoms/header/index.tsx
+++ src/ui/atoms/header/index.tsx
@@ -4,10 +4,10 @@ import style from "./index.module.scss";
 
 export type HeaderProps = {
     children: ReactNode;
-    fontsize?: number;
+    fontSize?: number;
 };
 
-export const Header: FC<HeaderProps> = ({ children, fontsize = 2 }) => (
+export const Header: FC<HeaderProps> = ({ children, fontSize = 2 }) => (
     <Flex
         direction={"row"}
         justify={"center"}
@@ -16,12 +16,12 @@ export const Header: FC<HeaderProps> = ({ children, fontsize = 2 }) => (
     >
         <hr
             className={style.line}
-            style={{ height: `${fontsize * 0.25}rem` }}
+            style={{ height: `${fontSize * 0.25}rem` }}
         />
-        <Paragraph fontSize={fontsize} className={style.text}>{children}</Paragraph>
+        <Paragraph fontSize={fontSize} className={style.text}>{children}</Paragraph>
         <hr
             className={style.line}
-            style={{ height: `${fontsize * 0.25}rem` }}
+            style={{ height: `${fontSize * 0.25}rem` }}
         />
     </Flex>
 );
--- src/ui/molecules/markdown_renderer/index.tsx
+++ src/ui/molecules/markdown_renderer/index.tsx
@@ -13,7 +13,7 @@ export type MarkdownRendererProps = {
 export const MarkdownRenderer: FC<MarkdownRendererProps> = ({ children, sizeMultiplier = 1 }) => (
     <Markdown components={{
         p: ({ children }) => <Paragraph fontSize={1.5 * sizeMultiplier}>{children}</Paragraph>,
-        h1: ({ children }) => <Header fontsize={2 * sizeMultiplier}>{children}</Header>,
+        h1: ({ children }) => <Header fontSize={2 * sizeMultiplier}>{children}</Header>,
     }}>
         {children}
     </Markdown>
