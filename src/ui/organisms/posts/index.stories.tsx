ad729d6c56c14f793785a7496bc1454fd1bd0127
Khenziii <khenzii.was.taken@gmail.com>
2024-10-26 10:26:28 +0200

    feat(ui): add story for the `Posts` component

---
+++ src/ui/organisms/posts/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Posts } from "@khenzii-dev/ui/organisms";
+import type { StoriesType } from "@khenzii-dev/ui/types";
+
+export default {
+    component: Posts,
+    title: 'Posts',
+    tags: ['autodocs'],
+};
+
+export const Default: StoriesType<Record<string, null>> = {
+    args: {},
+};
