796051f10461728fda9d1b37dcc0dcca4f73b7f3
Khenziii <khenzii.was.taken@gmail.com>
2025-08-14 15:35:42 +0200

    feat(ui): prevent `MarkdownRenderer` from losing state

--- src/ui/molecules/markdown_renderer/index.tsx
+++ src/ui/molecules/markdown_renderer/index.tsx
@@ -1,5 +1,5 @@
-import type { ReactElement, FC, ReactNode, CSSProperties } from "react";
-import Markdown from "react-markdown";
+import { type ReactElement, type FC, type ReactNode, type CSSProperties, useMemo } from "react";
+import Markdown, { type Options as MarkdownOptions } from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import {
     Paragraph,
@@ -41,72 +41,76 @@ export type MarkdownRendererProps = {
     sizeMultiplier?: number;
 };
 
-export const MarkdownRenderer: FC<MarkdownRendererProps> = ({ children, sizeMultiplier = 1 }) => (
-    <Markdown
-        rehypePlugins={[rehypeRaw]}
-        components={{
-            h1: ({ children }) => <Header fontSize={1.75 * sizeMultiplier}>{children}</Header>,
-            h2: ({ children }) => <Header variant={"secondary"} fontSize={1.5 * sizeMultiplier}>{children}</Header>,
-            h3: ({ children }) => <Header variant={"secondary"} fontSize={1.25 * sizeMultiplier}>{children}</Header>,
-            p: ({ children }) => {
-                const isImage = typeof children !== "string" && typeof (children as unknown[])?.[0] !== "string";
+export const MarkdownRenderer: FC<MarkdownRendererProps> = ({ children, sizeMultiplier = 1 }) => {
+    const components = useMemo<MarkdownOptions["components"]>(() => ({
+        h1: ({ children }) => <Header fontSize={1.75 * sizeMultiplier}>{children}</Header>,
+        h2: ({ children }) => <Header variant={"secondary"} fontSize={1.5 * sizeMultiplier}>{children}</Header>,
+        h3: ({ children }) => <Header variant={"secondary"} fontSize={1.25 * sizeMultiplier}>{children}</Header>,
+        p: ({ children }) => {
+            const isImage = typeof children !== "string" && typeof (children as unknown[])?.[0] !== "string";
 
-                return isImage
-                    ? children as unknown as ReactElement
-                    : <Paragraph styles={{ ...getCommonTextStyles(sizeMultiplier), fontWeight: 500 }}>{children}</Paragraph>;
-            },
-            img: ({ src, alt }) => (
-                <Multimedia additionalText={alt} sizeMultiplier={sizeMultiplier}>
-                    <img src={src} alt={alt} />
-                </Multimedia>
-            ),
-            video: ({ src, children }) => (
-                <Multimedia additionalText={children?.toString()} sizeMultiplier={sizeMultiplier}>
-                    <video controls>
-                        <source src={src} />
-                        {children?.toString()}
-                    </video>
-                </Multimedia>
-            ),
-            a: ({ children, href }) => (
-                <Anchor
-                    href={href ?? ""}
-                    styles={getCommonTextStyles(sizeMultiplier)}
-                    className={styles["common-text"]}
-                    darkenOnHover
-                    newTab
-                >
-                    {children}
-                </Anchor>
-            ),
-            ul: ({ children }) => <ul style={{ paddingLeft: 0 }}>{children}</ul>,
-            li: ({ children, node }) => (
-                <PrimitiveListItem
-                    level={node?.position?.start.column}
-                    styles={getCommonTextStyles(sizeMultiplier)}
-                    className={styles["common-text"]}
-                >
-                    {children}
-                </PrimitiveListItem>
-            ),
-            em: ({ children }) => (
-                <em
-                    style={getCommonTextStyles(sizeMultiplier)}
-                    className={styles["common-text"]}
-                >
-                    {children}
-                </em>
-            ),
-            strong: ({ children }) => (
-                <strong
-                    style={getCommonTextStyles(sizeMultiplier)}
-                    className={styles["common-text-strong"]}
-                >
-                    {children}
-                </strong>
-            ),
-        }}
-    >
-        {children}
-    </Markdown>
-);
+            return isImage
+                ? children as unknown as ReactElement
+                : <Paragraph styles={{ ...getCommonTextStyles(sizeMultiplier), fontWeight: 500 }}>{children}</Paragraph>;
+        },
+        img: ({ src, alt }) => (
+            <Multimedia additionalText={alt} sizeMultiplier={sizeMultiplier}>
+                <img src={src} alt={alt} />
+            </Multimedia>
+        ),
+        video: ({ src, children }) => (
+            <Multimedia additionalText={children?.toString()} sizeMultiplier={sizeMultiplier}>
+                <video controls>
+                    <source src={src} />
+                    {children?.toString()}
+                </video>
+            </Multimedia>
+        ),
+        a: ({ children, href }) => (
+            <Anchor
+                href={href ?? ""}
+                styles={getCommonTextStyles(sizeMultiplier)}
+                className={styles["common-text"]}
+                darkenOnHover
+                newTab
+            >
+                {children}
+            </Anchor>
+        ),
+        ul: ({ children }) => <ul style={{ paddingLeft: 0 }}>{children}</ul>,
+        li: ({ children, node }) => (
+            <PrimitiveListItem
+                level={node?.position?.start.column}
+                styles={getCommonTextStyles(sizeMultiplier)}
+                className={styles["common-text"]}
+            >
+                {children}
+            </PrimitiveListItem>
+        ),
+        em: ({ children }) => (
+            <em
+                style={getCommonTextStyles(sizeMultiplier)}
+                className={styles["common-text"]}
+            >
+                {children}
+            </em>
+        ),
+        strong: ({ children }) => (
+            <strong
+                style={getCommonTextStyles(sizeMultiplier)}
+                className={styles["common-text-strong"]}
+            >
+                {children}
+            </strong>
+        ),
+    }), [sizeMultiplier]);
+
+    return (
+        <Markdown
+            rehypePlugins={[rehypeRaw]}
+            components={components}
+        >
+            {children}
+        </Markdown>
+    );
+};
