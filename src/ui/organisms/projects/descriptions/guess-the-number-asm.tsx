db6a7906f8625de36c228f760314b4f8aef66081
Khenziii <khenzii.was.taken@gmail.com>
2025-09-05 21:45:50 +0200

    feat(ui): add `guess-the-number-asm` project's description

--- src/ui/organisms/projects/descriptions/guess-the-number-asm.tsx
+++ src/ui/organisms/projects/descriptions/guess-the-number-asm.tsx
@@ -1,8 +1,7 @@
 import { Paragraph } from "@khenzii-dev/ui/atoms";
 
-// TODO: ...
 export const GuessTheNumberAsmDescription = () => <>
     <Paragraph fontSize={1.25}>
-        TODO: finish writing this thing xD
+        {"Implementing a number guessing game (aka \"Higher Lower\") is a common learning exercise for beginner programmers. I did something similar, however in an old fashioned way. This projet was a helpful learning experience thanks to which I have a better understanding of all the commonly adopted software abstraction layers since the invention of computers. If you're on Linux, you can easily execute it yourself."}
     </Paragraph>
 </>;
