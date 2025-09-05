4aee58f8b8f26dd09edee731a6d3def4fd0670b6
Khenziii <khenzii.was.taken@gmail.com>
2025-09-05 22:53:07 +0200

    feat(ui): add `nixos-config` project's description

--- src/ui/organisms/projects/descriptions/nixos-config.tsx
+++ src/ui/organisms/projects/descriptions/nixos-config.tsx
@@ -1,8 +1,7 @@
 import { Paragraph } from "@khenzii-dev/ui/atoms";
 
-// TODO: ...
 export const NixosConfigDescription = () => <>
     <Paragraph fontSize={1.25}>
-        TODO: finish writing this thing xD
+        {"As a programmer, I'll probably spend many thousands of hours more behind my desk. Due to this, I wanted to keep track of my current working environment as accurately as posible to maximise my productivity. This is exactly what my nixos-config does. Thanks to it I can ensure that all of the apps I  need are intsalled, things look how I want them too, and much more."}
     </Paragraph>
 </>;
