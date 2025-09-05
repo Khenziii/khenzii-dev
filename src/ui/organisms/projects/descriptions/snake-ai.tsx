807f7df78b44343472a35313581f586f54cbd8d8
Khenziii <khenzii.was.taken@gmail.com>
2025-09-05 19:07:15 +0200

    feat(ui): add `snake-ai` project's description

--- src/ui/organisms/projects/descriptions/snake-ai.tsx
+++ src/ui/organisms/projects/descriptions/snake-ai.tsx
@@ -1,7 +1,13 @@
-import { Paragraph } from "@khenzii-dev/ui/atoms";
+import { Paragraph, Anchor } from "@khenzii-dev/ui/atoms";
 
 export const SnakeAiDescription = () => <>
     <Paragraph fontSize={1.25}>
-        {"This project was originally created as a "}
+        {"In the past couple of years, the tech industry has been heavily investing into AI research. Interested by this, I wanted to give implementing one a try. Obviously, a simple snake playing "}
+        <Anchor href={"https://en.wikipedia.org/wiki/Neural_network_(machine_learning)"} darkenOnHover newTab>{"neural network"}</Anchor>
+        {" differs a lot from giant "}
+        <Anchor href={"https://en.wikipedia.org/wiki/Large_language_model"} darkenOnHover newTab>{"LLMs"}</Anchor>
+        {" like "}
+        <Anchor href={"https://chatgpt.com"} darkenOnHover newTab>{"ChatGPT"}</Anchor>
+        {", but nevertheless this project has given me important insight."}
     </Paragraph>
 </>;
