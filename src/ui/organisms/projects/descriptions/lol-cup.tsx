1ac0f251f566cfca7eb74edac5bf73151d812570
Khenziii <khenzii.was.taken@gmail.com>
2025-09-06 18:17:11 +0200

    feat(ui): update `lol-cup` project's description

--- src/ui/organisms/projects/descriptions/lol-cup.tsx
+++ src/ui/organisms/projects/descriptions/lol-cup.tsx
@@ -8,51 +8,17 @@ const linkProps = {
 
 export const LolCupDescription = () => <>
     <Paragraph fontSize={1.25}>
-        {"If you're from "}
-        <Anchor href={"https://en.wikipedia.org/wiki/Danzig"} {...linkProps}>
-            Gdańsk
+        <Anchor href={"https://lolcup.zsl.gda.pl"} {...linkProps}>
+            {"ZSŁ LOL CUP"}
         </Anchor>
-        {", then you must've heard about "}
-        <Anchor href={"https://lolcup.zsl.gda.pl/"} {...linkProps}>
-            ZSŁ LOL CUP
+        {" is a "}
+        <Anchor href={"https://en.wikipedia.org/wiki/League_of_Legends"} {...linkProps}>
+            {"League of Legends"}
         </Anchor>
-        {"."}
-
-        <br />
-        <br />
-
-        {"It's a "}
-        <Anchor href={"https://www.leagueoflegends.com/"} {...linkProps}>
-            League of Legends
+        {" tournament organized by the students of "}
+         <Anchor href={"https://www.zsl.gda.pl"} {...linkProps}>
+            {"ZSŁ Gdańsk"}
         </Anchor>
-        {" tournament, that's being organized by students of "}
-        <Anchor href={"https://www.zsl.gda.pl/"} {...linkProps}>
-            ZSŁ
-        </Anchor>
-        {". LOL CUP is a yearly event, that has first appeared in 2017."}
-
-        <br />
-        <br />
-
-        {"I was lucky, to be able to help with organizing it's 7th edition."}
-
-        <br />
-        <br />
-
-        {"Alongside a couple of other devs, we've done a fair amount of rewriting & adding new features to the codebase."}
-
-        <br />
-        <br />
-
-        {"Overall, I have learned a lot of cool things, and I for sure recall helping out with the tournament as well-spent time :)"}
-
-        <br />
-        <br />
-
-        {"If you want to learn more about the tournament, and you're fluent in Polish, I recommend checking out "}
-        <Anchor href={"https://lolcup.zsl.gda.pl/o-turnieju"} {...linkProps}>
-            lolcup.zsl.gda.pl/o-turnieju
-        </Anchor>
-        {"."}
+        {" high school. I've had the chance to help with organizing its VIIth and VIIIth editions. In 2023/2024, I was a contributor helping to manage the old codebase, and the next year I became the lead developer."}
     </Paragraph>
 </>;
